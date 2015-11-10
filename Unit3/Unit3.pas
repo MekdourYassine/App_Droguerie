8aedf2b6d764e6648b504455868f7d1ad0092b64
MEKDOUR Yassine <ay_mekdour@esi.dz>
2015-11-10 18:41:44 +0100

    unité3

---
+++ Unit3/Unit3.pas
@@ -0,0 +1,33 @@
+unit Unit3;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, sSkinManager, StdCtrls, sLabel, ExtCtrls, sPanel;
+
+type
+  TForm3 = class(TForm)
+    sSkinManager1: TsSkinManager;
+    sPanel1: TsPanel;
+    sLabel1: TsLabel;
+    sLabel2: TsLabel;
+    sLabel3: TsLabel;
+    sLabel4: TsLabel;
+    sLabel5: TsLabel;
+    sLabel6: TsLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.dfm}
+
+end.
+ 
